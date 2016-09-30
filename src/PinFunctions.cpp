83e1083967ca0fea28b273c34a7e6d41731d996d
madhephaestus <mad.hephaestus@gmail.com>
2016-09-30 17:37:34 -0400

    no lockout of serial port modes

--- src/PinFunctions.cpp
+++ src/PinFunctions.cpp
@@ -43,10 +43,10 @@ boolean setMode_Local(uint8_t pin, uint8_t mode) {
 			println_I("Detaching servo");
 			myservo[PIN_TO_SERVO(pin)].detach();
 		}
-		if((GetChannelMode(pin) == IS_DEBUG_TX||GetChannelMode(pin) == IS_DEBUG_RX)&&!startupFlag){
-			println_W("Bailing because this mode NOT changeable");
-			return true;
-		}
+// 		if((GetChannelMode(pin) == IS_DEBUG_TX||GetChannelMode(pin) == IS_DEBUG_RX)&&!startupFlag){
+// 			println_W("Bailing because this mode NOT changeable");
+// 			return true;
+// 		}
 
 		_EEWriteMode(pin, mode);
 		//getBcsIoDataTable(pin)->PIN.currentChannelMode = mode;
